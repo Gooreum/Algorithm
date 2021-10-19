772ff72bc5d08bc57bfab045c6450388a7143b4e
Mingu Seo <tjalsrnek@gmail.com>
2021-10-19 15:05:27 +0900

    longest_common_prefix 문제 코드

---
+++ LeetCode.playground/Pages/14.Longest_Common_Prefix.xcplaygroundpage/Contents.swift
@@ -0,0 +1,17 @@
+//[question] https://leetcode.com/problems/longest-common-prefix/
+
+import Foundation
+
+
+func longestCommonPrefix(_ strs: [String]) -> String {
+    if strs.isEmpty { return "" }
+    var common = strs[0]
+    strs.forEach({
+        while !$0.hasPrefix(common) {            
+            common = String(common.dropLast())
+        }
+    })
+    return common
+}
+
+print(longestCommonPrefix(["flower","flow","flight"]))
